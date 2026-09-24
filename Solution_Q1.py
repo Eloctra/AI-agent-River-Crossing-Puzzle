4ca69579adf609f05bdefce9e08c73d9ea16e162
Eloctra <karthikekrishnan@gmail.com>
2026-09-24 00:50:18 -0400

    Implemented BFS/DFS. Tested, fixed typos and readlines->readline

--- Solution_Q1.py
+++ Solution_Q1.py
@@ -2,7 +2,7 @@
 
 def parse_input(path):
     file = open(path,"r")
-    line = file.readlines().strip()
+    line = file.readline().strip()
     file.close()
 
     parts = [part.strip() for part in line.split(',')]
@@ -69,7 +69,7 @@ def state_str(state):
     return f"({str(m_left)},{str(c_left)},{str(m_right)},{str(c_right)},{boat})"
 
 class Node:
-    __parts__=("state","parten","action","cost")
+    __parts__=("state","parent","action","cost")
 
     def __init__(self,state,parent=None,action=None,cost=0):
         self.state=state
@@ -84,8 +84,67 @@ def build_path(node):
          node=node.parent
     chain.reverse()
 
-    parts = [state_str(chain[0].state)]           #Start - > action-state -> action-state
+    parts = [state_str(chain[0].state)]           #Start - > action-state -> action-state...
     for n in chain[1:]:
         parts.append(action_string(n.action))    
         parts.append(state_str(n.state))
     return " ".join(parts)
+
+
+def graph_search(initial_state:tuple ,stack:bool):
+    root = Node(initial_state)
+
+    if goal(root.state):
+        return root
+
+    fringe=[root]
+    explored = set()
+    n_expansions=0
+
+    while fringe:
+        if stack:
+            node=fringe.pop()  #LIFO
+        else:
+            node = fringe.pop(0)  #FIFO
+
+        if node.state in explored:
+            continue
+        explored.add(node.state)
+        n_expansions+=1
+
+        for action,next_state,step_cost in successor_state(node.state):
+            if next_state in explored:
+                continue
+            child = Node(next_state, parent=node,action=action,cost=node.cost+step_cost)
+            if goal(child.state):
+                return child,n_expansions
+            fringe.append(child)
+    return None,n_expansions
+
+def dfs(initial_state):
+    return graph_search(initial_state,True)
+def bfs (initial_state):
+    return graph_search(initial_state,False)
+
+
+
+#test : -
+def run(question:str,search,initial_state:tuple):
+    goal,expansions=search(initial_state)
+    print("The solution of " + question + " is:")
+    if goal is None:
+        print("Solution Path: No Solution")
+        print("Total Cost: NA")
+        print("Number of Node Expansions =" +str(expansions))
+    else:
+        print("Solution Path: " + build_path(goal))
+        print("Total Cost: "+ str(goal.cost))
+        print("Number of Node Expansions =" +str(expansions))
+
+def main():
+    initial_state=parse_input("input.txt")
+    run("Q1.1.a (DFS)",dfs,initial_state)
+    run("Q1.1.b (BFS)",bfs,initial_state)
+
+if __name__=="__main__":
+    main()
